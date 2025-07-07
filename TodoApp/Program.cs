fea38884fb02a2b1f02e74af84b88f6fbc9c2ca7
AstralWanderer <renshi0226@gmail.com>
2025-07-07 23:21:40 +0800

    Enabled authentication and updated connection string for Identity

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -22,8 +22,11 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
