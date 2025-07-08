dd6d7be51f60f86c782b1e99f31a41bb28d66d65
AstralWanderer <renshi0226@gmail.com>
2025-07-08 15:32:13 +0800

    Create and registered email sender service

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using TodoApp.Areas.Identity.Data;
+using TodoApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("TodoAppIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'TodoAppIdentityDbContextConnection' not found.");;
 
@@ -10,6 +13,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
---
+++ TodoApp/Services/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using MailKit;
+using MailKit.Security;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+using MimeKit.Text;
+
+namespace TodoApp.Services;
+
+public class EmailSender : IEmailSender
+{
+    private readonly IConfiguration _config;
+
+    public EmailSender(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("TodoApp", _config["EmailSettings:SmtpUser"]));
+        message.To.Add(MailboxAddress.Parse(email));
+        message.Subject = subject;
+        message.Body = new TextPart("html"){ Text = htmlMessage };
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync(
+            _config["EmailSettings:SmtpServer"],
+            int.Parse(_config["EmailSettings:SmtpPort"]),
+            SecureSocketOptions.StartTls
+        );
+        await client.AuthenticateAsync(
+            _config["EmailSettings:SmtpUser"],
+            _config["EmailSettings:SmtpPass"]
+        );
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
