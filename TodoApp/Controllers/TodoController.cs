62f4682c573cfdb96b03f1fab5a5b665d4cfff01
Renshi Seña <renshi0226@gmail.com>
2025-07-13 23:28:57 +0800

    Added Todo Controller

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Controllers;
+
+public class TodoController : Controller
+{
+    public IActionResult Create()
+    {
+        throw new NotImplementedException();
+    }
+}
